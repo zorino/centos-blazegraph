4651dbc58ff80a2fe42e36d1f5febeef9edb308a
Maxime Déraspe <maximilien1er@gmail.com>
2015-07-27 14:56:20 -0400

    fix Load-Graph.sh no docker in cmd, common
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- Load-Graph.sh
+++ Load-Graph.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cmd=$1
-sudo docker exec e5c26e6bc484 curl -XPOST --data-urlencode "$cmd" http://localhost:9999/bigdata/sparql
-
+curl -XPOST --data-urlencode "$cmd" http://localhost:9999/bigdata/sparql
 
